92b1c6d021c461b12c48f5bfffa6cd4d566b66b3
LLOlaBB <lauracbarros@hotmail.com>
2023-03-02 19:20:49 -0300

    Adicionado try catch

--- src/Lista3_Repetição/Lista3Ex5.java
+++ src/Lista3_Repetição/Lista3Ex5.java
@@ -1,21 +1,28 @@
 package Lista3_Repetição;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class Lista3Ex5 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int i = 0;
         int soma = 0;
+        try {
+            for (i = 1; i <= 10; i++) {
+                System.out.println("Digite o " + i + "º número: ");
+                int num = scan.nextInt();
+                soma += num;
+            }
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("Digite o " + i + "º número: ");
-            int num = scan.nextInt();
-            soma += num;
+            System.out.println("A soma dos " + i + " números é: " + soma);
+        } catch (InputMismatchException e) {
+            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
         }
-
-        System.out.println("A soma dos 10 números é: " + soma);
-
         scan.close();
     }
 }
